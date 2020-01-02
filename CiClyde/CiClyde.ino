6f6464e19317e50857e3b59ad75b60cd2b73cd61
ssalanski <stefan.salanski@gmail.com>
2020-01-01 23:30:41 -0500

    add function to change color based on given success/fail

--- CiClyde/CiClyde.ino
+++ CiClyde/CiClyde.ino
@@ -6,6 +6,17 @@
 
 #define FIRMWARE_VERSION 1
 
+#define COLOR_SUCCESS RGB(32,230,150)
+#define COLOR_FAIL    RGB(255,32,0)
+
+void updateColor(boolean buildSuccess) {
+  if(buildSuccess) {
+    Clyde.fadeAmbient(COLOR_SUCCESS, 0.5f);
+  } else {
+    Clyde.fadeAmbient(COLOR_FAIL, 0.5f);
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   Clyde.begin();
